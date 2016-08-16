aa7b9099c18ecd0e305929e04b692ae0240c41f9
10891 <fei.tang@hand-china.com>
2016-08-16 12:30:49 +0800

    第二版
    解决运行失败问题

--- SpringMybatisExam/src/main/java/com/hand/App.java
+++ SpringMybatisExam/src/main/java/com/hand/App.java
@@ -3,17 +3,22 @@ package com.hand;
 import com.hand.dto.Address;
 import com.hand.service.AddressService;
 
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+
     public static void main( String[] args )
     {
-    	System.out.println(System.getProperty("user.dir"));
+//    	System.out.println(System.getProperty("user.dir"));
     	AddressService addser = new AddressService();
         Address add = addser.addressIsExist(5) ;
         if(add != null){ System.out.println(add.getAddress());}
+        
+    	
     }
 }
--- SpringMybatisExam/src/main/java/com/hand/service/AddressService.java
+++ SpringMybatisExam/src/main/java/com/hand/service/AddressService.java
@@ -1,11 +1,7 @@
 package com.hand.service;
 
-import java.io.Reader;
-
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hand.dto.Address;
 import com.hand.serviceInter.AddressInter;
@@ -13,30 +9,22 @@ import com.hand.serviceInter.AddressInter;
 
 public class AddressService {
 
-	private static SqlSessionFactory sqlSessionFactory;
-	private static Reader reader;
-	static{
-		try{
-			reader = Resources.getResourceAsReader("config/ApplicationContext.xml");
-			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+   	private static ApplicationContext ctx;
+	static
+	{
+		ctx = new ClassPathXmlApplicationContext("config/ApplicationContext.xml");
 	}
 	
 	public Address addressIsExist(int id){
-		SqlSession session = sqlSessionFactory.openSession();
 		Address address = null;
 		try {
-			AddressInter addressInter = session.getMapper(AddressInter.class);
-			address = addressInter.getAddressById(id);
+    		AddressInter addressMapper =  (AddressInter) ctx.getBean("addressMapper");
+    		address = addressMapper.getAddressById(id);
 			if(address == null){
 				return null;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
-			session.close();
 		}
 		return address;
 	}
